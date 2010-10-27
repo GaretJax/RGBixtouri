e519e90becacc1233a216a2cbb974b1c15e7a575
Stéphane <Stphane@160.98.61.175>
2010-10-27 14:50:07 +0200

    First skeleton of application

--- src/rgbixtouri/alpha/alphaLayoutManager/RGBixtouri.java
+++ src/rgbixtouri/alpha/alphaLayoutManager/RGBixtouri.java
@@ -9,6 +9,12 @@ import javax.swing.JPanel;
 import com.atticlabs.zonelayout.swing.ZoneLayout;
 import com.atticlabs.zonelayout.swing.ZoneLayoutFactory;
 
+/**
+ * 
+ * @author Stéphane
+ * Main class of application. Main frame containing other panels.
+ * 
+ */
 public class RGBixtouri extends JFrame{
   
   private ImageLibraryPanel imageSelectorPanel;
@@ -17,51 +23,56 @@ public class RGBixtouri extends JFrame{
   private GraphesPanel graphesPanel;
   private ClustersPanel clustersPanel;
   
+  /**
+   * Constructor
+   */
   public RGBixtouri(){
     this.buildUI();
     this.setVisible(true);
   }
   
+  /**
+   * Build the user interface of the frame
+   */
   private void buildUI(){
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     
+    //Zone layout is the layout manager used
     ZoneLayout zLayout = ZoneLayoutFactory.newZoneLayout();
     zLayout.addRow("B+I+IG+G");
     zLayout.addRow(".BP+PC+C");
     zLayout.compile();
     
     zLayout.getZone("B").setTake(0, 100);
-    zLayout.getZone("I").setTake(50, 100);
+    zLayout.getZone("I").setTake(100, 100);
     zLayout.getZone("G").setTake(100, 100);
     zLayout.getZone("C").setTake(100, 100);
     zLayout.getZone("P").setTake(50, 100);
     this.setLayout(zLayout);
+    
     setSize(new Dimension(800, 600));
     setMinimumSize(new Dimension(400,300));
     
+    //creation and setting of the panels
     imageSelectorPanel = new ImageLibraryPanel();
-    imageSelectorPanel.setBackground(Color.red);
-    imageSelectorPanel.setPreferredSize(new Dimension(100,200));
+    imageSelectorPanel.setPreferredSize(new Dimension(200,200));
     imagePanel = new ImagePanel();
-    imagePanel.setBackground(Color.black);
+    imagePanel.setPreferredSize(new Dimension(300,300));
     parametersPanel = new ParametersPanel();
-    parametersPanel.setBackground(Color.green);
+    parametersPanel.setPreferredSize(new Dimension(300,150));
     graphesPanel = new GraphesPanel();
-    graphesPanel.setBackground(Color.blue);
+    graphesPanel.setPreferredSize(new Dimension(300,300));
     clustersPanel = new ClustersPanel();
-    clustersPanel.setBackground(Color.yellow);
+    clustersPanel.setPreferredSize(new Dimension(300,150));
     
     this.add(imageSelectorPanel, "B");
     this.add(imagePanel, "I");
     this.add(parametersPanel, "P");
     this.add(graphesPanel, "G");
     this.add(clustersPanel, "C");
-    
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
     new RGBixtouri();
   }
-
 }
