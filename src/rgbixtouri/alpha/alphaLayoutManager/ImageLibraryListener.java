a1c1308fcd76a6bc23a89b70e2b3d452b1660575
Stéphane <Stphane@pobiolls08-In>
2010-10-28 09:41:12 +0200

    Creation

---
+++ src/rgbixtouri/alpha/alphaLayoutManager/ImageLibraryListener.java
@@ -0,0 +1,19 @@
+package rgbixtouri.alpha.alphaLayoutManager;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ImageLibraryListener implements PropertyChangeListener{
+	
+	private ImageLibraryPanel imageLibrary;
+	
+	public ImageLibraryListener(ImageLibraryPanel view){
+		this.imageLibrary=view;
+	}
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		imageLibrary.imageList.updateUI();
+	}
+
+}
