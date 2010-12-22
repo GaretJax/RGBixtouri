36d77c40bedbc4c4ee036637695fd78d951e73fa
Frederic Monney <stephane.biolley@edu.hefr.ch>
2010-12-22 16:24:01 +0100

    i18n

--- src/gui/ZoneStatePanel.java
+++ src/gui/ZoneStatePanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.ResourceBundle;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -11,11 +12,13 @@ import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.TableColumn;
 
+import language.LanguageUpdate;
+import language.LanguageUpdater;
 import model.ImageModel;
 import model.ImageModel.Zone;
 import model.ZoneTableModel;
 
-public class ZoneStatePanel extends JPanel implements Observer {
+public class ZoneStatePanel extends JPanel implements Observer, LanguageUpdate {
 
 	/**
 *
@@ -25,6 +28,8 @@ public class ZoneStatePanel extends JPanel implements Observer {
 	private JTable zonesTableSkin;
 	private JScrollPane scrollPaneWound;
 	private JScrollPane scrollPaneSkin;
+	
+	private ImageModel im;
 
 	public ZoneStatePanel() {
 		Dimension screen = new Dimension(this.getWidth(), 50);
@@ -43,10 +48,19 @@ public class ZoneStatePanel extends JPanel implements Observer {
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		this.add(scrollPaneWound, BorderLayout.NORTH);
 		this.add(scrollPaneSkin, BorderLayout.SOUTH);
+		
+		
+		LanguageUpdater lu = LanguageUpdater.getInstanceOfLanguageUpdater();
+		lu.addObject(this);
+		
+		zonesTableWound.setModel(new ZoneTableModel(null));
+		zonesTableSkin.setModel(new ZoneTableModel(null));
+		
 	}
 
 	public void selectedImageChanged(ImageModel is) {
 		is.addObserver(this);
+		im = is;
 		zonesTableWound.setModel(new ZoneTableModel(is.getArea(Zone.WOUND)));
 		zonesTableSkin.setModel(new ZoneTableModel(is.getArea(Zone.SKIN)));
 	}
@@ -54,6 +68,18 @@ public class ZoneStatePanel extends JPanel implements Observer {
 	@Override
 	public void update(Observable selection, Object _) { // selection of type
 															// ImageModel
+	}
+
+	@Override
+	public void updateLanguage(ResourceBundle resourceBundle) {
+		if(im != null){
+			zonesTableWound.setModel(new ZoneTableModel(im.getArea(Zone.WOUND)));
+			zonesTableSkin.setModel(new ZoneTableModel(im.getArea(Zone.SKIN)));
+		}else{
+			zonesTableWound.setModel(new ZoneTableModel(null));
+			zonesTableSkin.setModel(new ZoneTableModel(null));
+		}
+		
 	}
 
 }
