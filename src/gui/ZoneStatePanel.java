b021e20ee1ab268ec03e00a07d9c86ba303b4e3f
Frederic Monney <stephane.biolley@edu.hefr.ch>
2010-12-22 16:20:43 +0100

    .

--- src/gui/ZoneStatePanel.java
+++ src/gui/ZoneStatePanel.java
@@ -15,42 +15,45 @@ import model.ImageModel;
 import model.ImageModel.Zone;
 import model.ZoneTableModel;
 
-public class ZoneStatePanel extends JPanel implements Observer{
+public class ZoneStatePanel extends JPanel implements Observer {
 
-/**
+	/**
 *
 */
-private static final long serialVersionUID = 1L;
-private JTable zonesTableWound;
-private JTable zonesTableSkin;
-private JScrollPane scrollPaneWound;
-private JScrollPane scrollPaneSkin;
+	private static final long serialVersionUID = 1L;
+	private JTable zonesTableWound;
+	private JTable zonesTableSkin;
+	private JScrollPane scrollPaneWound;
+	private JScrollPane scrollPaneSkin;
 
-public ZoneStatePanel(){
-Dimension screen = new Dimension(this.getWidth(), 50);
-this.setLayout(new BorderLayout());
-zonesTableWound=new JTable();
-zonesTableWound.setModel(new ZoneTableModel(null));
-zonesTableWound.setPreferredScrollableViewportSize(screen);
-zonesTableSkin=new JTable();
-zonesTableSkin.setModel(new ZoneTableModel(null));
-zonesTableSkin.setPreferredScrollableViewportSize(screen);
-scrollPaneWound=new JScrollPane(zonesTableWound, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-scrollPaneSkin= new JScrollPane(zonesTableSkin, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-this.add(scrollPaneWound, BorderLayout.NORTH);
-this.add(scrollPaneSkin, BorderLayout.SOUTH);
+	public ZoneStatePanel() {
+		Dimension screen = new Dimension(this.getWidth(), 50);
+		this.setLayout(new BorderLayout());
+		zonesTableWound = new JTable();
+		zonesTableWound.setModel(new ZoneTableModel(null));
+		zonesTableWound.setPreferredScrollableViewportSize(screen);
+		zonesTableSkin = new JTable();
+		zonesTableSkin.setModel(new ZoneTableModel(null));
+		zonesTableSkin.setPreferredScrollableViewportSize(screen);
+		scrollPaneWound = new JScrollPane(zonesTableWound,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPaneSkin = new JScrollPane(zonesTableSkin,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		this.add(scrollPaneWound, BorderLayout.NORTH);
+		this.add(scrollPaneSkin, BorderLayout.SOUTH);
+	}
+
+	public void selectedImageChanged(ImageModel is) {
+		is.addObserver(this);
+		zonesTableWound.setModel(new ZoneTableModel(is.getArea(Zone.WOUND)));
+		zonesTableSkin.setModel(new ZoneTableModel(is.getArea(Zone.SKIN)));
+	}
+
+	@Override
+	public void update(Observable selection, Object _) { // selection of type
+															// ImageModel
+	}
+
 }
-
-public void selectedImageChanged(ImageModel is){
-is.addObserver(this);
-zonesTableWound.setModel(new ZoneTableModel(is.getArea(Zone.WOUND)));
-zonesTableSkin.setModel(new ZoneTableModel(is.getArea(Zone.SKIN)));
-}
-
-@Override
-public void update(Observable selection, Object _) { //selection of type ImageModel
-}
-
-
-}
-
